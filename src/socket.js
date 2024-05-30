eecc1fd45ccbf220b1a5cc9e72d2c421055b2969
Apurba-Kumar-Ghosh <theshield0979@gmail.com>
2024-05-31 05:23:25 +0530

    Send socket req to deployed server link

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,9 @@
 import React, { useContext, useRef } from "react";
 import { io } from "socket.io-client";
 
-const deployedServerURL = "https://rock-paper-scissor-game-ioii.onrender.com";
+const SERVER_URL = "https://rock-paper-scissor-game-ioii.onrender.com";
 
-const URL =
-  process.env.NODE_ENV === "production" ? deployedServerURL : deployedServerURL;
-
-export const socketInstance = io(URL);
+export const socketInstance = io(SERVER_URL);
 
 const SocketContext = React.createContext(undefined);
 
